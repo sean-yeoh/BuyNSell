f644a1b3ac1c26e2aa3a55615ea82b3598bf5787
Sean Yeoh <sean.yeoh@hotmail.com>
2016-12-21 14:19:46 +0800

    Added bootstrap rails gem Create authentication with bcrypt Add users controller, allow user to signup Add sessions controller, allow user to login Drop database and change users table 'name' column to 'username' Add navbar Add helper method for bootstrap alert

--- db/schema.rb
+++ db/schema.rb
@@ -17,7 +17,7 @@ ActiveRecord::Schema.define(version: 20161221031235) do
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
-    t.string   "name",            null: false
+    t.string   "username",        null: false
     t.string   "email",           null: false
     t.string   "password_digest", null: false
     t.datetime "created_at",      null: false
